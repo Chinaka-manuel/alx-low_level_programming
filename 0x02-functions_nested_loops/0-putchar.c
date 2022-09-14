25356fead6ea06809f6fb4a21cf740ffa933ef89
Chinaka-manuel <chinakamauel@gmail.com.com>
2022-09-13 18:07:06 -0700

    a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "code.h" 
 
 /**
  * main - Prints "code" followed by a new line.
  *
  * Return: Always 0.
  */
-int main(void)
+	int main(void)
 {
 	_putchar('c');
 	_putchar('o');
